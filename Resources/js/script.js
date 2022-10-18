9ce07040e03cc898fb0f48fdf3fa313fc48eb7f5
Sabbir Hossain Abir <2020-2-60-185@std.ewubd.edu>
2022-10-19 01:19:32 +0600

    made the sticky navigation dynamic

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
 
-    
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+            offset: '80px;'
+    });
 
 
 });
