224bfab1e234e212c2c9089712e37cacab41d693
Sabbir Hossain Abir <2020-2-60-185@std.ewubd.edu>
2022-10-19 02:40:00 +0600

    added animation

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
             offset: '80px;'
     });
 
+
     /*-- Scroll on buttons --*/
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);       
@@ -58,5 +59,31 @@ $(document).ready(function () {
     }
   });
     
+    
+    /*-- Animations --*/
+    
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
 
 });
